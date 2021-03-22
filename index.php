4a858783436ae4b44d277010873ad7f5cbf198db
Oleg9791 <oleg9791@tut.by>
2021-03-22 22:29:46 +0300

    add

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Опрос</title>
+    <!-- Подключение css Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- Подключение обнуляющего css -->
+    <link rel="stylesheet" href="css/zeroing.css">
+    <!-- Подключение css со своими стилями -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Подключение адаптивного css -->
+    <link rel="stylesheet" href="css/adaptive.css">
+</head>
+
+<body>
+    <!-- Блок header -->
+    <header>
+        <!-- Подключение container Bootstrap -->
+        <div class="container">
+            <a href="#" class="logo">
+                <img src="css/img/logo2.png" alt="Логотип">
+            </a>
+            <ul class="mnu__top">
+                <li class="mnu__top-link"><a href="#">Главная</a></li>
+                <li class="mnu__top-link"><a href="#">О компании</a></li>
+                <li class="mnu__top-link"><a href="#">Услуги</a></li>
+                <li class="mnu__top-link"><a href="#">Контакты</a></li>
+            </ul>
+        </div>
+    </header>
+    <!-- Первый блок section -->
+    <section id="main" style="background-image:url(https://images.unsplash.com/photo-1512621425532-58df884d97f0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80)">
+        <div class="container">
+            <div class="inner">
+                <h1>Услуги такси</h1>
+                <div class="description">Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.
+                    Назад о, пустился рукопись рыбными журчит последний, не океана моей всемогущая точках пунктуация lorem там,
+                    первую меня инициал дал безопасную?<br>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.
+                    Назад о, пустился рукопись рыбными журчит последний, не океана моей всемогущая точках пунктуация lorem там,
+                    первую меня инициал дал безопасную?<br></div>
+                <a class="btn" href="#">Отправить заявку</a>
+            </div>
+        </div>
+    </section>
+    <section class="registr">
+        <!-- подключение container Bootstrap -->
+        <div class="container">
+            <div class="registr__inner">
+                <div class="registr__content">
+                    <form class="registr__form" action="add.php" method="POST">
+                        <h3 class="registr__title">ЗАРЕГИСТРИРУЙСЯ И ПРОЙДИ ОПРОС</h3>
+                        <button class="registr__form-btn" type="submit" tabindex="3"><a href="login.html" class="btn btn-success">Войти</a></button>
+                        <button class="registr__form-btn" type="submit" tabindex="3"><a href="registration.html" class="btn btn-success">Регистрация</a></button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class="inner__footer">
+                <ul>
+                    <li><a href="#">Пункт меню</a></li>
+                    <li><a href="#">Пункт меню</a></li>
+                    <li><a href="#"><img src="css/img/fb.png"></a></li>
+                    <li><a href="#">Пункт меню</a></li>
+                    <li><a href="#">Пункт меню</a></li>
+                    <li><a href="#"><img src="css/img/instagram.png"></a></li>
+                    <li><a href="#">Пункт меню</a></li>
+                    <li><a href="#">Пункт меню</a></li>
+                    <li><a href="#"><img src="css/img/email.png"></a></li>
+                    <li><a href="#">Пункт меню</a></li>
+                    <li><a href="#">Пункт меню</a></li>
+                    <li><a href="#"><img src="css/img/Twitter.png"></a></li>
+                </ul>
+                <div class="footer__text">
+                    Онлайн-опрос © 2021 ZaicevOleg Все права защищены
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- подключение js Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- testresult.php
+++ testresult.php
@@ -1,3 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+</body>
+</html>
 <?php
 $q11 = "Как часто Вы пользуетесь услугами такси?";
 $q22 = "По какой причине Вы чаще всего пользуетесь услугами такси?";
@@ -12,13 +29,16 @@ echo " <th colspan='5' style='border: none'>Вопросы и ответы </th>
 foreach ($data as $row) {
 
 
-    echo "<tr><td>$q11<br> $row[q1]</td><td> $q22<br>$row[q2]</td><td>$q33<br>$row[q3]</td>
-<td>$q44<br>$row[q4]</td><td>$q55<br>$row[q5]</td></tr>";
+    echo "<tr><td><b>$q11</b><br><i>$row[q1]</i></td><td><b>$q22</b><br><i>$row[q2]</i>></td><td><b>$q33</b><br><i>$row[q3]</i></td>
+<td><b>$q44</b><br><i>$row[q4]</i>></td><td><b>$q55</b><br><i>$row[q5]</i></td></tr>";
 
 
 }
 //echo ($data[0]);
 echo "</table>";
-
+?>
+<a class="btn btn-warning" href="index.html">Вернуться на главную страницу</a>
+</body>
+</html>
 
 
