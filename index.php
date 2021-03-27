156a26ac65f2ad95ed1b6cbfc5787eeceb775c24
Oleg9791 <oleg9791@tut.by>
2021-03-27 21:38:37 +0300

    add

--- index.php
+++ index.php
@@ -90,3 +90,236 @@
 </body>
 
 </html>
+<!--<!DOCTYPE html>-->
+<!--<html lang="en">-->
+<!---->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+<!--    <title>Опрос</title>-->
+<!--    <!-- Подключение css Bootstrap -->-->
+<!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"-->
+<!--          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
+<!--    <!-- https://cdnjs.com/ -->-->
+<!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"-->
+<!--          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="-->
+<!--          crossorigin="anonymous" />-->
+<!--    <!-- Подключение обнуляющего css  -->-->
+<!--<!--    <link rel="stylesheet" href="css/zeroing.css">-->-->
+<!--<!--    <!-- Свои стили -->-->-->
+<!--<!--    <link rel="stylesheet" href="css/style.css">-->-->
+<!--<!--    <!-- Адаптив -->-->-->
+<!--<!--    <link rel="stylesheet" href="css/adaptive.css">-->-->
+<!--</head>-->
+<!---->
+<!--<body>-->
+<!--<!-- header -->-->
+<!--<div class="wrapper">-->
+<!--    <header class="header">-->
+<!--        <div class="menu">-->
+<!--            <div class="container">-->
+<!--                <div class="menu__container">-->
+<!--                    <div class="menu__logo">-->
+<!--                        <img class="menu__logo-img" src="images/logo.png" alt="Логотип">-->
+<!--                    </div>-->
+<!--                    <ul class="menu__list">-->
+<!--                        <li class="menu__item">-->
+<!--                            <a class="menu__link" href="#">Home</a>-->
+<!--                        </li>-->
+<!--                        <li class="menu__item">-->
+<!--                            <a class="menu__link" href="#">About</a>-->
+<!--                        </li>-->
+<!--                        <li class="menu__item">-->
+<!--                            <a class="menu__link" href="#">Servicing</a>-->
+<!--                        </li>-->
+<!--                        <li class="menu__item">-->
+<!--                            <a class="menu__link" href="#">Portfolio</a>-->
+<!--                        </li>-->
+<!--                        <li class="menu__item">-->
+<!--                            <a class="menu__link" href="#">Blog</a>-->
+<!--                        </li>-->
+<!--                        <li class="menu__item">-->
+<!--                            <a class="menu__link" href="#">Contact Us</a>-->
+<!--                        </li>-->
+<!--                        <li class="menu__item"><a class="menu__link" href="#">-->
+<!--                                <!-- Подключение иконки с https://fontawesome.com/ -->-->
+<!--                                <i class="fas fa-search"></i>-->
+<!--                            </a>-->
+<!--                        </li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <!-- Подключение иконки с https://fontawesome.com/ -->-->
+<!--        <div class="hero__arrow-down">-->
+<!--            <i class="fas fa-chevron-down"></i>-->
+<!--        </div>-->
+<!--    </header>-->
+<!--    <section class="slider">-->
+<!--        <!-- Подключение Bootstrap -->-->
+<!--        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">-->
+<!--            <div class="carousel-inner">-->
+<!--                <div class="carousel-item active" id="main">-->
+<!--                    <img src="images/taksi.jpg" class="d-block w-100" alt="...">-->
+<!--                </div>-->
+<!--                <div class="carousel-item" id="main">-->
+<!--                    <img src="images/taksi2.jpg" class="d-block w-100" alt="...">-->
+<!--                </div>-->
+<!--                <div class="carousel-item" id="main">-->
+<!--                    <img src="images/taksi3.jpg" class="d-block w-100" alt="...">-->
+<!--                </div>-->
+<!--                <!-- Текст и кнопки на слайде -->-->
+<!--                <div class="hero">-->
+<!--                    <div class="hero__container">-->
+<!--                        <h1 class="hero__title">Lorem, ipsum dolor</h1>-->
+<!--                        <h3 class="hero__subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>-->
+<!--                        <a class="hero__btn one" href="login.html">Sign in</a>-->
+<!--                        <a class="hero__btn two" href="registration.html">Sign up</a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"-->
+<!--                    data-bs-slide="prev">-->
+<!--                <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+<!--                <span class="visually-hidden">Previous</span>-->
+<!--            </button>-->
+<!--            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"-->
+<!--                    data-bs-slide="next">-->
+<!--                <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+<!--                <span class="visually-hidden">Next</span>-->
+<!--            </button>-->
+<!--        </div>-->
+<!--    </section>-->
+<!--    <section class="price">-->
+<!--        <!-- Подключение container Bootstrap -->-->
+<!--        <div class="container">-->
+<!--            <div class="title">-->
+<!--                <h2 class="h2 title__h2">Pricing Plan</h2>-->
+<!--                <p class="h2 title__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, in</p>-->
+<!--            </div>-->
+<!--            <ul class="price__list">-->
+<!--                <li class="price__item">-->
+<!---->
+<!--                    <div class="price__top">-->
+<!--                        <h3 class="price__title">Free Trail</h3>-->
+<!--                        <div class="price__num">-->
+<!--                            <span class="price__price">00</span>-->
+<!--                            <span class="price__period">per month</span>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="price__bottom">-->
+<!--                        <ul class="price__options">-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                        </ul>-->
+<!--                        <a class="price__btn" href="#"> Lorem ipsum</a>-->
+<!--                    </div>-->
+<!--                </li>-->
+<!--                <li class="price__item">-->
+<!---->
+<!--                    <div class="price__top">-->
+<!--                        <h3 class="price__title">Basic</h3>-->
+<!--                        <div class="price__num">-->
+<!--                            <span class="price__price">50</span>-->
+<!--                            <span class="price__period">per month</span>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="price__bottom">-->
+<!--                        <ul class="price__options">-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                        </ul>-->
+<!--                        <a class="price__btn" href="#"> Lorem ipsum</a>-->
+<!--                    </div>-->
+<!--                </li>-->
+<!--                <li class="price__item">-->
+<!---->
+<!--                    <div class="price__top">-->
+<!--                        <h3 class="price__title">Ultimates</h3>-->
+<!--                        <div class="price__num">-->
+<!--                            <span class="price__price">99</span>-->
+<!--                            <span class="price__period">per month</span>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="price__bottom">-->
+<!--                        <ul class="price__options">-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                            <li class="price__elem">Lorem, ipsum dolor</li>-->
+<!--                        </ul>-->
+<!--                        <a class="price__btn" href="#"> Lorem ipsum</a>-->
+<!--                    </div>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--    </section>-->
+<!--    <!-- <section class="contact">-->
+<!--        <div class="container">-->
+<!--            <div class="title contact__title">-->
+<!--                <h2 class="h2 title__h2">Contact Us</h2>-->
+<!--                <p class="h2 title__content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea neque-->
+<!--                    voluptates illum quidem et dignissimos at! Voluptates velit eum quis?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, quisquam!-->
+<!--                </p>-->
+<!--            </div>-->
+<!--            <ul class="contact__list">-->
+<!--                <li class="contact__item">-->
+<!--                    <i class="fas fa-map-marker-alt contact__icon"></i>-->
+<!--                    <p class="contact__text">-->
+<!--                        Республика Беларусь-->
+<!--                    </p>-->
+<!--                </li>-->
+<!--                <li class="contact__item">-->
+<!--                    <i class="fas fa-phone-alt contact__icon"></i>-->
+<!--                    <p class="contact__text">-->
+<!--                        +x xxx xxx xx xx-->
+<!--                    </p>-->
+<!--                    <p class="contact__text">-->
+<!--                        +x xxx xxx xx xx-->
+<!--                    </p>-->
+<!--                </li>-->
+<!--                <li class="contact__item">-->
+<!--                    <i class="fas fa-envelope contact__icon"></i>-->
+<!--                    <p class="contact__text">-->
+<!--                        Lorem@lorem.com-->
+<!--                    </p>-->
+<!--                    <p class="contact__text">-->
+<!--                        Lorem@lorem.com-->
+<!--                    </p>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--            <div class="contact__form">-->
+<!--                <form class="form" action="" method="POST" >-->
+<!--                    <p class="form__title">Войти под правами администратора</p>-->
+<!--                    <input class="form__input" type="text" name="" placeholder="Email" autofocus tabindex="1">-->
+<!--                    <input class="form__input" type="text" name="" placeholder="Password" tabindex="2">-->
+<!--                    <button class="form__btn" type="submit">Войти</button>-->
+<!--                </form>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </section>-->
+<!--    <footer class="footer">-->
+<!--<div class="container">-->
+<!--<div class="inner__footer">-->
+<!--    <div class="footer__text">-->
+<!--        Онлайн-опрос © 2021 ZaicevOleg Все права защищены-->
+<!--    </div>-->
+<!--</div>-->
+<!--</div>-->
+<!--    </footer>-->
+<!--</div> -->-->
+<!--    <!-- Подключение js Bootstrap -->-->
+<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"-->
+<!--            integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"-->
+<!--            crossorigin="anonymous"></script>-->
+<!--</body>-->
+<!---->
+<!--</html>-->
